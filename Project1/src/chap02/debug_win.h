de3464c02c69bd33cfc5b7c3b4f85ad02cc7a374
Masanori Igarashi <masanori.igarashi1@gmail.com>
2022-11-29 09:49:47 +0900

    Move ThrowIfFailed() to inline function

--- Project1/src/chap02/debug_win.h
+++ Project1/src/chap02/debug_win.h
@@ -1,9 +1,37 @@
 #pragma once
 #include <windows.h>
+#include <exception>
+#include <cstdio>
 
 namespace Dbg {
 	inline void assert(bool b);
-	void ThrowIfFailed(HRESULT hr);
 	void print(const char* format, ...);
 	void printLastError();
+
+	// Helper class for COM exceptions
+	class com_exception : public std::exception
+	{
+	public:
+		com_exception(HRESULT hr) : result(hr) {}
+
+		const char* what() const override
+		{
+			static char s_str[64] = {};
+			sprintf_s(s_str, "Failure with HRESULT of %08X",
+				static_cast<unsigned int>(result));
+			return s_str;
+		}
+
+	private:
+		HRESULT result;
+	};
+
+    // Helper utility converts D3D API failures into exceptions.
+    inline void ThrowIfFailed(HRESULT hr)
+    {
+        if (FAILED(hr))
+        {
+            throw com_exception(hr);
+        }
+    }
 }
