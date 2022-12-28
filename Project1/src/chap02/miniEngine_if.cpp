ad3ed373ed31209c1517cb61dd69267f647b5b9a
Masanori Igarashi <masanori.igarashi1@gmail.com>
2022-12-28 13:45:17 +0900

    updated project file to refer DirectXTex.lib

---
+++ Project1/src/chap02/miniEngine_if.cpp
@@ -0,0 +1,9 @@
+#include "miniEngine_if.h"
+#include "../../import/hlsl-grimoire-sample/MiniEngine/MiniEngine.h"
+
+#pragma comment(lib, "hlsl-grimoire-sample_miniEngine.lib")
+
+void test()
+{
+	MeshParts meshParts;
+}
