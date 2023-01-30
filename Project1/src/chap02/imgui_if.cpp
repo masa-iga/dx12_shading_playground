36a9ecd231bd483eb941901eef883469997f3163
Masanori Igarashi <masanori.igarashi1@gmail.com>
2023-01-30 09:50:20 +0900

    just started make imgui lib

---
+++ Project1/src/chap02/imgui_if.cpp
@@ -0,0 +1,11 @@
+#include "imgui_if.h"
+#include "../../import/imgui/imgui.h"
+
+#pragma comment(lib, "imgui.lib")
+
+namespace ImguiIf {
+	void init()
+	{
+		;
+	}
+}
---
+++ Project1/src/chap02/imgui_if.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace ImguiIf {
+	void init();
+}
