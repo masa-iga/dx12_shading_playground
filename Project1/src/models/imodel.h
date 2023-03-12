1a1df58bdb2b30f788edbda6ab250f5aa66ba850
Masanori Igarashi <masanori.igarashi1@gmail.com>
2023-03-12 15:44:06 +0900

    updated model_07_02

--- Project1/src/models/imodel.h
+++ Project1/src/models/imodel.h
@@ -7,6 +7,7 @@ class IModels {
 public:
 	virtual ~IModels() = 0;
 	virtual void createModel() = 0;
+	virtual void resetCamera() = 0;
 	virtual void handleInput() = 0;
 	virtual void debugRenderParams() { }
 	void draw(RenderContext& renderContext);
@@ -20,6 +21,5 @@ class IModelFactory {
 public:
 	virtual ~IModelFactory() = 0;
 	virtual std::unique_ptr<IModels> create() = 0;
-	virtual void setCamera3D() { }
 };
 
--- Project1/src/models/model_07_02.cpp
+++ Project1/src/models/model_07_02.cpp
@@ -13,40 +13,74 @@ class ModelFactory_07_02 : public IModelFactory
 public:
 	~ModelFactory_07_02() { }
 	std::unique_ptr<IModels> create();
-	void setCamera3D();
 };
 
 class Models_07_02 : public IModels
 {
 public:
-	struct Light {
+	struct DirectionalLight {
 		Vector3 direction;
-		float pad0 = 0.0f;
+		float pad0;
 		Vector4 color;
+	};
+
+	struct Light {
+		static constexpr uint32_t kNumDirectionalLight = 4;
+
+		DirectionalLight directionalLight[kNumDirectionalLight];
 		Vector3 eyePos;
-		float specRow = 0.0f;
+		float specPow;
 		Vector3 ambientLight;
-		int32_t enableAmbientMap = 0;
+		float pad0;
 	};
 
 public:
 	Models_07_02() { }
 	~Models_07_02() { }
 	void createModel();
+	void resetCamera();
 	void handleInput();
-	void debugRenderParams();
-	void setLight(Light light) { m_light = light; }
+	void setLight(const Light& light) { m_light = light; }
 	Light* getLightPtr() { return &m_light; }
+	void debugRenderParams();
 
 private:
-	const std::string kTkmFile = "Sample_06_03/Sample_06_03/Assets/modelData/sample.tkm";
-	const std::string kFxFile = "Assets/shader/sample_06_03.fx";
-	const std::wstring kAoFile = L"Sample_06_03/Sample_06_03/Assets/modelData/guitar_01_AO.dds";
+	const std::string kTkmFile = "Sample_07_02/Sample_07_02/Assets/modelData/monster.tkm";
+	const std::string kFxFile = "Assets/shader/sample_07_02.fx";
 	std::string getTkmFilePath() { return ModelUtil::getPathFromAssetDir(kTkmFile); }
 	std::string getFxFilePath() { return kFxFile; }
-	std::wstring getAoFilePath() { return ModelUtil::getPathFromAssetDirWstr(kAoFile); }
 
-	Texture m_aoMap;
+	struct DispSetting
+	{
+		Vector3 cameraPos;
+		Vector3 cameraTarget;
+	};
+
+	enum class Character : uint32_t
+	{
+		kMonster,
+		kHuman,
+		kLantern,
+		kNum,
+	};
+
+	const DispSetting kDispSettings[static_cast<uint32_t>(Character::kNum)] =
+	{
+		{
+			.cameraPos = { 0.0f, 85.0f, 50.0f },
+			.cameraTarget = { 0.0f, 85.0f, 0.0f },
+		},
+		{
+			.cameraPos = { 0.0f, 160.0f, 50.0f },
+			.cameraTarget = { 0.0f, 160.0f, 0.0f },
+		},
+		{
+			.cameraPos = { 0.0f, 50.0f, 120.0f },
+			.cameraTarget = { 0.0f, 50.0f, 0.0f },
+		},
+	};
+
+	Character m_dispModelNo = Character::kMonster;
 	Light m_light;
 };
 
@@ -54,16 +88,43 @@ std::unique_ptr<IModels> ModelFactory_07_02::create()
 {
 	std::unique_ptr<Models_07_02> m(new Models_07_02);
 	{
-		Models_07_02::Light light = {
-			.direction = { 1.0f, -1.0f, -1.0f },
-			.pad0 = 0.0f,
-			.color = { 0.0f, 0.0f, 0.0f, 0.0f },
-			.eyePos = MiniEngineIf::getCamera3D()->GetPosition(),
-			.specRow = 5.0f,
-			.ambientLight = { 1.0f, 1.0f, 1.0f },
-			.enableAmbientMap = 1,
-		};
-		m->setLight(light);
+		{
+			Models_07_02::Light light = {
+				.directionalLight = {
+					{
+						.direction = { 2.0f, -1.0f, 3.0f},
+						.pad0 = 0.0f,
+						.color = { 3.0f, 3.0f, 3.0f, 0.0f },
+					},
+					{
+						.direction = { 0.0f, 0.0f, -1.0f},
+						.pad0 = 0.0f,
+						.color = { 1.5f, 1.5f, 1.5f, 0.0f },
+					},
+					{
+						.direction = { 0.0f, 0.0f, 0.0f},
+						.pad0 = 0.0f,
+						.color = { 0.0f, 0.0f, 0.0f, 0.0f },
+					},
+					{
+						.direction = { 0.0f, 0.0f, 0.0f},
+						.pad0 = 0.0f,
+						.color = { 0.0f, 0.0f, 0.0f, 0.0f },
+					},
+				},
+				.eyePos = MiniEngineIf::getCamera3D()->GetPosition(),
+				.specPow = 0.5f,
+				.ambientLight = { 0.4f, 0.4f, 0.4f },
+				.pad0 = 0.0f,
+			};
+
+			for (auto& dirLight : light.directionalLight)
+			{
+				dirLight.direction.Normalize();
+			}
+
+			m->setLight(light);
+		}
 
 		m->createModel();
 
@@ -71,25 +132,25 @@ std::unique_ptr<IModels> ModelFactory_07_02::create()
 		{
 			model->UpdateWorldMatrix(g_vec3Zero, g_quatIdentity, g_vec3One);
 		}
+
+		m->resetCamera();
 	}
 
 	return m;
 }
 
-void ModelFactory_07_02::setCamera3D()
+void Models_07_02::resetCamera()
 {
-	MiniEngineIf::getCamera3D()->SetPosition({ 0.0f, 130.0f, 300.0f });
-	MiniEngineIf::getCamera3D()->SetTarget({ 0.0f, 130.0f, 0.0f });
+	MiniEngineIf::getCamera3D()->SetPosition(kDispSettings[static_cast<int32_t>(m_dispModelNo)].cameraPos);
+	MiniEngineIf::getCamera3D()->SetTarget(kDispSettings[static_cast<int32_t>(m_dispModelNo)].cameraTarget);
 }
 
 void Models_07_02::createModel()
 {
 	const std::string tkmFilePath = getTkmFilePath();
 	const std::string fxFilePath = getFxFilePath();
-    const std::wstring aoFilePath = getAoFilePath();
 	Dbg::assert_(std::filesystem::exists(tkmFilePath));
 	Dbg::assert_(std::filesystem::exists(fxFilePath));
-	Dbg::assert_(std::filesystem::exists(aoFilePath));
 
 	ModelInitData initData = { };
 	{
@@ -97,13 +158,11 @@ void Models_07_02::createModel()
 		initData.m_fxFilePath = fxFilePath.c_str();
 		initData.m_expandConstantBuffer = &m_light;
 		initData.m_expandConstantBufferSize = sizeof(m_light);
-		m_aoMap.InitFromDDSFile(aoFilePath.c_str());
-		initData.m_expandShaderResoruceView.at(0) = &m_aoMap;
 	}
 
 	std::unique_ptr<Model> model(new Model);
 	model->Init(initData);
-	m_models.push_back(std::move(model));
+	m_models.emplace_back(std::move(model));
 }
 
 void Models_07_02::handleInput()
@@ -120,7 +179,10 @@ void Models_07_02::handleInput()
 			qRot.SetRotationDegY(-1.0f);
 		}
 
-		qRot.Apply(m_light.direction);
+		for (auto& lig : m_light.directionalLight)
+		{
+			qRot.Apply(lig.direction);
+		}
 	}
 
 	{
@@ -149,22 +211,25 @@ void Models_07_02::handleInput()
 	m_light.eyePos = MiniEngineIf::getCamera3D()->GetPosition();
 
 	{
-		if (MiniEngineIf::isPress(MiniEngineIf::Button::kA))
+		if (MiniEngineIf::isTrigger(MiniEngineIf::Button::kA))
 		{
-			m_light.enableAmbientMap = 1;
-		}
-		else if (MiniEngineIf::isPress(MiniEngineIf::Button::kB))
-		{
-			m_light.enableAmbientMap = 0;
+			m_dispModelNo = static_cast<Character>((static_cast<int32_t>(m_dispModelNo) + 1) % static_cast<int32_t>(Character::kNum));
+			resetCamera();
 		}
 	}
 }
 
 void Models_07_02::debugRenderParams()
 {
-	ImguiIf::printParams<float>(ImguiIf::VarType::kFloat, "Direct light", std::vector<float*>{ &(m_light.direction.x), & (m_light.direction.y), & (m_light.direction.z) });
+	ImguiIf::printParams<int32_t>(ImguiIf::VarType::kInt32, "Camera", std::vector<int32_t*>{ (int32_t*)&m_dispModelNo });
 	ImguiIf::printParams<float>(ImguiIf::VarType::kFloat, "Eye         ", std::vector<float*>{ &(m_light.eyePos.x), & (m_light.eyePos.y), & (m_light.eyePos.z) });
-	ImguiIf::printParams<int32_t>(ImguiIf::VarType::kInt32, "SpecularMap ", std::vector<int32_t*>{ &(m_light.enableAmbientMap)});
+
+	for (uint32_t i = 0; i < _countof(m_light.directionalLight); ++i)
+	{
+		DirectionalLight& dl = m_light.directionalLight[static_cast<size_t>(i)];
+		std::string str = "Direct light" + std::to_string(i);
+		ImguiIf::printParams<float>(ImguiIf::VarType::kFloat, str, std::vector<float*>{ &(dl.direction.x), & (dl.direction.y), & (dl.direction.z) });
+	}
 }
 
 namespace ModelHandler {
@@ -173,7 +238,6 @@ namespace ModelHandler {
 	void loadModelForChap07_02(std::vector<Model*>& models)
 	{
 		ModelFactory_07_02 factory;
-		factory.setCamera3D();
 		m = factory.create();
 		m->debugRenderParams();
 		models = m->getModels();
